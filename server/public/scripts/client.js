e0a60e9067171c9ae0d3b0577e7c35b8da2ff2f1
Amy Smith <smithniamyfse@gmail.com>
2023-06-25 00:58:41 -0500

    updated client.js, todlist-router.js, and index.html to reflect the different methods of alterting data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,19 +3,173 @@ console.log('client.js initialized');
 $(document).ready(onReady);
 
 // function onReady is a callback function
-    // it is a function that is handed to 
-    // some other function to be executed later on.
+// it is a function that is handed to
+// some other function to be executed later on.
 function onReady() {
-    console.log('jQuery is initialized');
-    // TODO: create getCurrentToDoList function 
-        // getCurrentToDoList();
+  console.log('jQuery is initialized');
+  // call getCurrentToDoList to get the task list on initial page load
+  getCurrentToDoList();
 
-    // Event Listener and register an event handler for adding a task button
-    // $('#add-task-button').on('click', addTask);
+  // Event Listener and register an event handler for high-priority checkbox
+  $('#checkbox-high-priority').on('click', updateToHighPriority);
 
-    // Event Listener and register an event handler for high-priority checkbox
-    // $('#checkbox-high-priority').on('click', updateToHighPriority);
+  // Event Listener and register an event handler for adding a task button
+  $('#add-task-button').on('click', addTask);
 
-    // Event listener and register an event handler for deleting a task button
-    // $('#delete-task-button').on('click', deleteTask);
+  // Event Listener and register an event handler for completion status button
+  $('#complete-task-button').on('click', completeTask);
+
+  // Event listener and register an event handler for deleting a task button
+  $('#delete-task-button').on('click', deleteTask);
+}
+
+// TODO: $.ajax Functions
+
+// ** function getCurrentToDoList returns the list of tasks at the
+// beginning of page rendering and refreshes the task list to reflect any changes
+// * GET METHOD retrieves a resource
+function getCurrentToDoList() {
+  console.log('in getCurrentToDoList');
+  // ajax call to server to get the task list
+  $.ajax({
+    type: 'GET',
+    url: '/todolist',
+  }).then((response) => {
+    console.log('GET /todolist response:', response);
+    // Pass the response data to the render function
+    render(response);
+  });
+} // end getCurrentToDoList
+
+// ** function addTask adds a task to the task list
+// and refreshes the task list to include the new task
+// * POST METHOD creates a new resource
+function addTask() {
+  console.log('in addTask');
+  // Collect user input and put values in an object
+  let newTask = {
+    task_is_high_priority: $('#checkbox-high-priority').val(),
+    task: $('#task-in').val(),
+  };
+
+  $.ajax({
+    type: 'POST',
+    url: '/todolist',
+    data: newTask,
+  })
+    .then(function (response) {
+      console.log('Response from server: ', response);
+      $('#checkbox-high-priority').val(''), $('#task-in').val('');
+      getCurrentToDoList();
+    })
+    .catch(function (error) {
+      console.log('Error in POST - adding a task to the list', error);
+      alert('Unable to add a new task at this time. Please try again later.');
+    });
+} // end addTask
+
+
+// ** function updateToHighPriority changes the task to high priority
+    // * PUT METHOD updates an existing resource
+  function updateToHighPriority() {
+    console.log('in updateToHighPriority');
+    console.log('The check-marked task is: ', $(this));
+    const taskId = $(this).parent().parent().data("id");
+    console.log('taskId of check-marked task is: ', taskId);
+  
+    $.ajax({
+      method: 'PUT',
+      url: `/todolist/${taskId}`,
+    })
+      .then((response) => {
+        console.log('Task is checked as high priority! Response: ', response);
+        getCurrentToDoList();
+      })
+      .catch((error) => {
+        console.log('Error in check-marking high priority', error);
+        alert('Task has NOT been updated to high priority.');
+        res.sendStatus(500);
+      });
+  } // end updateToHighPriority
+
+
+
+// ** function completeTask updates the task to completed
+// * PUT METHOD updates an existing resource
+  function completeTask() {
+    console.log('in completeTask');
+    console.log('The completed task is: ', $(this));
+    const taskId = $(this).parent().parent().data("id");
+    console.log('taskId of completed task is: ', taskId);
+  
+    $.ajax({
+      method: 'PUT',
+      url: `/todolist/${taskId}`,
+    })
+      .then((response) => {
+        console.log('Task has been completed! Response: ', response);
+        getCurrentToDoList();
+      })
+      .catch((error) => {
+        console.log('Error in changing task to completed', error);
+        alert('Task has NOT been changed to completed.');
+        res.sendStatus(500);
+      });
+  } // end completeTask
+
+
+// ** function deleteTask deletes a task with a given id from the task list
+// and refreshes the task list to show the task has been removed
+// * DELETE METHOD deletes a resource
+function deleteTask() {
+  // Log the specific id of the task to delete
+  console.log('in deleteTask: ', $(this));
+
+  // Use DOM traversal to get the data id of the koalas table row
+  const taskId = $(this).parent().parent().data('id');
+
+  // Send a delete request to the server
+  $.ajax({
+    method: 'DELETE',
+    url: `/todolist/${taskId}`,
+  })
+    .then((response) => {
+      console.log('Deleted the task from the list', response);
+      getCurrentToDoList();
+    })
+    .catch((error) => {
+      console.log('Error in DELETE - deleting the task from the list', error);
+      // Notifies the user with an alert window
+      alert('Error with deleting this task from the list.');
+    });
+}
+
+// ** function render takes the raw data and converts it into something visible to users
+// for this web-app it involves HTML and DOM manipulation
+function render(tasks) {
+  $('#view-to-do-list').empty();
+  // loop through the tasks
+  for (let i = 0; i < tasks.length; i++) {
+    //   if (`${tasks[i].ready_to_transfer}` == "N") {
+    $('#view-to-do-list').append(`
+      <tr data-id=${tasks[i].id}>
+      <td>${tasks[i].task_is_high_priority}</td>
+      <td>${tasks[i].task_is_complete}</td>
+      <td>${tasks[i].task}</td>
+      <td><button id='delete-task-button'>Delete</button></td>
+  </tr>
+      `);
+    //       } else {
+    //         $("#viewKoalas").append(`
+    //     <tr data-id=${koalas[i].id}>
+    //     <td>${koalas[i].name}</td>
+    //     <td>${koalas[i].age}</td>
+    //     <td>${koalas[i].gender}</td>
+    //     <td>${koalas[i].ready_to_transfer}</td>
+    //     <td>${koalas[i].notes}</td>
+    //     <td>Ready to Transfer! :)</td>
+    //     <td><button class='delete-button'>Delete</button></td>
+    //   </tr>
+    //     `);
+  }
 }
--- server/routes/todolist-router.js
+++ server/routes/todolist-router.js
@@ -1,6 +1,145 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
+// GET all tasks from the 'weekend_to_do_app' database
+router.get('/', (req, res) => {
+  // write SQL query and save that in a variable
+  const queryText = 'SELECT * FROM "todolist";';
+  // send SQL query to the database using pool.query
+  pool.query(queryText)
+    // best practice is to use the word 'result' to describe
+    // what we get back from the 'weekend_to_do_app' database
+    .then((result) => {
+      console.log('result is:', result);
+      // result.rows is where the data we requested is
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('Error making query:', error);
+      res.sendStatus(500);
+    });
+}); // end GET all tasks from the database
 
+
+// POST a new task to the 'weekend_to_do_app' database
+router.post('/', (req, res) => {
+    //  Create a constant that is an object that stores information in key-value pairs,
+        // and subsequently sends it to the Express server
+    const newTask = req.body;
+    console.log('Adding a new task', newTask);
+    // SLQ query to insert a new item into the todolist table
+    // use parameterization to eliminate SQL injection
+    const queryText = `INSERT INTO "todolist" ("task_is_high_priority", "task_is_complete", "task")
+                        VALUES ($1, $2, $3);`;
+  
+    const taskParams = [
+      newTask.task_is_high_priority,
+      newTask.task_is_complete,
+      newTask.task,
+    ];
+  
+    // Use pool as the communication line to handle the query
+    pool.query(queryText, taskParams)
+      // Get the result of query
+      .then((result) => {
+        // Send a status code of 201 (Created) to indicate the client-side request
+            // was successful and created the new task
+        res.sendStatus(201);
+      })
+      // Catch any errors that occur
+      .catch((error) => {
+        console.log('Error making database query: ', error);
+        res.sendStatus(500);
+      });
+  }); // end router.post 
+
+/*
+// GET specific koala by ID
+// id is a route parameter
+// we use this parameter to identify
+// that we want this specific identified part of the request
+router.get("/:id", (req, res) => {
+  // assign req.params.id to a variable
+  // write a SQL query that gets back the koala with the
+  // specified (parameter) id
+  const idToGet = req.params.id;
+
+  const query = 'SELECT * FROM "KoalasWebApp" WHERE id = $1;';
+  // use pool.query to access pool
+  // (group of connections between server and database)
+  pool
+    .query(query, [idToGet])
+    // .then because query is asynchronous
+    .then((result) => {
+      console.log(`Koala with id of ${idToGet}:`, result.rows);
+      // result.rows is where the desired part of the
+      // result/response is
+      // send data back to client
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log("Error making database query:", error);
+      res.sendStatus(500);
+    });
+}); // end GET specific koala by ID
+
+
+
+// PUT todolist task_is_high_priority value to 'TRUE' for a specific ID
+router.put("/:id", (req, res) => {
+  const idToUpdate = req.params.id;
+  const query = `UPDATE "KoalasWebApp" SET "ready_to_transfer" = 'Y' WHERE "id" = $1;`;
+
+  pool
+    .query(query, [idToUpdate])
+    .then((result) => {
+      console.log("Koala is ready to transfer!", result);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("Error changing Koala transfer status:", error);
+      res.sendStatus(500);
+    });
+});
+
+// PUT todolist task_is_complete value to 'TRUE' for a specific ID
+router.put("/:id", (req, res) => {
+  const idToUpdate = req.params.id;
+  const query = `UPDATE "KoalasWebApp" SET "ready_to_transfer" = 'Y' WHERE "id" = $1;`;
+
+  pool
+    .query(query, [idToUpdate])
+    .then((result) => {
+      console.log("Koala is ready to transfer!", result);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("Error changing Koala transfer status:", error);
+      res.sendStatus(500);
+    });
+});
+
+// DELETE a koala by ID
+router.delete("/:id", (req, res) => {
+  const idToDelete = req.params.id;
+  // Use query parameterization to protect the
+  // database from SQL injection
+  const query = `DELETE FROM "KoalasWebApp" WHERE "id" = $1`;
+
+  // Connect/talk with the database and run the query
+  pool
+    .query(query, [idToDelete])
+    .then((result) => {
+      console.log("Koala deleted");
+      // Send status 200 or 'ok' to the client
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("Error making database query:", error);
+      // Send status code 500 or 'internal server error'
+      res.sendStatus(500);
+    });
+});
+*/
 module.exports = router;
